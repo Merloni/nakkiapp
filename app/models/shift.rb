50dd19cb6cde42e6d075fb2fee7ea526b5d60b74
Merloni <tuomo.salmenkivi@gmail.com>
2016-05-15 15:58:55 +0300

    validations

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,4 +2,12 @@ class Shift < ActiveRecord::Base
   belongs_to :task
   belongs_to :user
 
+
+  def value
+    if self.hour > 9
+      s = "" << self.hour.to_s << ".00"
+    else
+      s = "0" << self.hour.to_s << ".00"
+    end
+  end
 end
